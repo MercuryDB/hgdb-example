f434154611a02c2376f2c3e946b6fa4947c1e1d4
colestewart <tcstewart09@gmail.com>
2015-04-22 23:40:50 -0500

    finished phase 1

--- phase-1/src/main/java/examples/app/Main.java
+++ phase-1/src/main/java/examples/app/Main.java
@@ -3,10 +3,24 @@ package examples.app;
 import examples.db.PersonTable;
 import examples.schema.Person;
 import org.mercurydb.queryutils.HgDB;
+import org.mercurydb.queryutils.HgRelation;
 import org.mercurydb.queryutils.HgStream;
+import org.mercurydb.queryutils.TableID;
 
+/**
+ * Phase 1 of Gradle People example. Can be ran by running "gradle run"
+ *
+ * Created 4/22/2015
+ */
 public class Main {
+
+    public static final TableID<Person> PERSON_ALIAS = PersonTable.createAlias();
+
     public static void main(String[] args) {
+
+        // Our nice little database. The constructor bytecode hooks will insert the
+        // people into the database!
+
         Person[] people = {
                 new Person("John Doe", 20, true),
                 new Person("Rhianne Styles", 21, false),
@@ -14,7 +28,48 @@ public class Main {
                 new Person("Doug Ilijev", 24, true),
         };
 
+        System.out.println("The Database:");
+        PersonTable.stream().forEach(System.out::println);
+
+        // ===========================
+        // Single Table Filter Queries
+        // ===========================
+
+        //  Query all people with ages >= 21
+        System.out.println("\nages >= 1:");
         HgStream<Person> stream = HgDB.query(PersonTable.ge.age(21));
         stream.forEachRemaining(System.out::println);
+
+        //  Query all people with first names that begin with a letter after I
+        System.out.println("\nname >= 'I':");
+        HgDB.query(PersonTable.ge.name("I")).forEachRemaining(System.out::println);
+
+        // Query all people that are male and less than 23
+        System.out.println("\nmale and age < 23:");
+        HgDB.query(
+                PersonTable.eq.gender(Person.Gender.MALE),
+                PersonTable.lt.age(23))
+                .forEachRemaining(System.out::println);
+
+        // Query all people whose age is divisible by 2
+        System.out.println("\nage % 2 == 0:");
+        HgDB.query(PersonTable.predicate(p -> p.getAge() % 2 == 0))
+                .forEachRemaining(System.out::println);
+
+
+        // ==========
+        // Self Joins
+        // ==========
+
+        // Query pairs (p1, p2) of all people where age of p1 < age of p2
+        //
+        // note: referencing the PersonTable.ID here is redundant. The statement
+        //       could have also been written as PersonTable.on.age()
+        System.out.println("\np1.age < p2.age:");
+        HgDB.join(
+                PersonTable.as(PersonTable.ID).on.age(),
+                PersonTable.as(PERSON_ALIAS).on.age(),
+                HgRelation.LT)
+        .forEachRemaining(System.out::println);
     }
 }
