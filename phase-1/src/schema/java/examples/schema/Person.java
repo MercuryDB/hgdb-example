a85db1c0a6a56d7ab29ae66bd41ba7cc5843933d
Doug Ilijev <doug.ilijev@gmail.com>
2015-05-14 22:57:27 -0500

    update phase-1 schema

--- phase-1/src/schema/java/examples/schema/Person.java
+++ phase-1/src/schema/java/examples/schema/Person.java
@@ -4,30 +4,27 @@ import com.github.mercurydb.annotations.HgIndexStyle;
 import com.github.mercurydb.annotations.HgUpdate;
 import com.github.mercurydb.annotations.HgValue;
 
+@SuppressWarnings("unused")
 public class Person {
+    // declare private state first, public fields discouraged
+
     private String name;
     private int age;
-    private boolean gender; // female is true, male is false
+    private Gender gender;
 
-    public Person(String name, int age, boolean gender) {
+    // declare all constructors
+
+    public Person(String name, int age, Gender gender) {
         this.name = name;
         this.age = age;
         this.gender = gender;
     }
 
-    public String toString() {
-        return String.format("{name=%s, age=%d, gender=%s}",
-                name, age, gender ? "female" : "male");
-    }
+    // declare all @HgValues after constructors so they are easy to find
 
     @HgValue(value = "name", index = HgIndexStyle.ORDERED)
     public String getName() {
         return name;
-    }
-
-    @HgUpdate("name")
-    public void setName(String name) {
-        this.name = name;
     }
 
     @HgValue(value = "age", index = HgIndexStyle.ORDERED)
@@ -35,41 +32,53 @@ public class Person {
         return age;
     }
 
-    @HgUpdate("age")
-    public void setAge(int age) {
-        this.age = age;
+    @HgValue(value = "canDrink", index = HgIndexStyle.UNORDERED)
+    public boolean canDrink() {
+        return getAge() >= 21;
+    }
+
+    @HgValue(value = "canSmoke", index = HgIndexStyle.UNORDERED)
+    public boolean canSmoke() {
+        return getAge() >= 18;
     }
 
     @HgValue(value = "gender", index = HgIndexStyle.UNORDERED)
     public Gender getGender() {
-        if (gender) {
-            return Gender.FEMALE;
-        } else {
-            return Gender.MALE;
-        }
+       return gender;
+    }
+
+    // declare all @HgUpdates after @HgValues so they are grouped together
+
+    @HgUpdate("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Update the age of this Person. Multiple @HgValues depend on
+     * the value of age (canSmoke and canDrink), so annotate that this
+     * method updates all of them.
+     *
+     * @param age The new age.
+     */
+    @HgUpdate({"age", "canSmoke", "canDrink"})
+    public void setAge(int age) {
+        this.age = age;
     }
 
     @HgUpdate("gender")
     public void setGender(Gender gender) {
-        switch (gender) {
-            case MALE:
-                this.gender = false;
-                break;
-            case FEMALE:
-                this.gender = true;
-                break;
-            default:
-                break;
-        }
+        this.gender = gender;
     }
 
-    @HgUpdate({"name", "age", "gender"})
-    public void copyFrom(Person o) {
-    	this.name = o.name;
-    	this.age = o.age;
-    	this.gender = o.gender;
+    // declare any other helper code or methods that are not @HgValues or @HgUpdates
+
+    public String toString() {
+        return String.format("{name=%s, age=%d, gender=%s}",
+                name, age, gender);
     }
 
+    @SuppressWarnings("unused")
     public enum Gender {
         MALE,
         FEMALE,
